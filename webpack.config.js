01df4d156170daf0bd760e02c52e587c4f256e05
Takuya Abe <abe_takuya@cocone.co.jp>
2019-12-05 00:37:05 +0900

    feat: Install 'webpack-dev-server'.

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,24 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = (env, options) => {
+    const mode = options.mode;
+    const isDevelopMode = mode === 'development';
+    const output = isDevelopMode ? './public/' : './dist/';
+    const outputPath = path.resolve(__dirname, output);
     return {
         entry: {
             'assets/scripts/index': path.join(__dirname, 'src/assets/scripts/index.ts')
+        },
+        output: {
+            path: outputPath,
+            publicPath: '/',
+            filename: '[name].js'
+        },
+        devServer: {
+            contentBase: path.resolve(__dirname, './public/'),
+            watchContentBase: true,
+            open: true,
+            port: 9000
         },
         plugins: [
             new HtmlWebpackPlugin({
