7d2cfa4f3ef2b90069b8765deadf4cb4aaf97325
Takuya Abe <abe_takuya@cocone.co.jp>
2019-12-05 00:26:22 +0900

    feat: Install 'html-loader' and 'html-webpack-plugin'.

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = (env, options) => {
     return {
         entry: {
             'assets/scripts/index': path.join(__dirname, 'src/assets/scripts/index.ts')
-        }
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            })
+        ]
     }
 }
